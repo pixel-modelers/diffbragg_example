a13bd344c940c1df467a07a6129b7e2c475742f5
dermen <dermen@lbl.gov>
2025-10-15 18:52:18 -0700

    updates and cleanup

--- dbex/data_load.py
+++ dbex/data_load.py
@@ -60,7 +60,7 @@ class DataLoad:
         # Get the raw image data from the experiment
         self.data = utils.image_data_from_expt(self.Expt)
         """
-        A :py:class:`flex.double` array of the raw image pixel values.
+        A :py:class:`np.ndarray` array of the raw image pixel values.
         """
 
         # Get the pixel data and background estimates near the reflections
@@ -69,60 +69,28 @@ class DataLoad:
                 self.Refs, self.data, use_robust_estimation=False, shoebox_sz=12,
                 reject_roi_with_hotpix=False,
                 pad_for_background_estimation=3, hotpix_mask=self.data < 0, weighted_fit=False)
-        #self.bbox
-        #"""
-        #A list/array of the **bounding boxes** (regions of interest) for each
-        #reflection on the detector.
-        #"""
-        #self.pids
-        #"""
-        #An array of **panel IDs** indicating which detector panel each reflection
-        #is located on.
-        #"""
-        #self.tilt_coefs
-        #"""
-        #The **coefficients** (e.g., from a plane fit) used for the background
-        #estimation model near the reflections.
-        #"""
-        #self.bg_is_good
-        #"""
-        #A **boolean array** flagging whether the background estimation was
-        #considered successful/reliable for each reflection.
-        #"""
-        #self.background_image
-        #"""
-        #A representation of the **estimated background** pixels for the regions
-        #of interest.
-        #"""
-
-
-
-
-#from simtbx.diffBragg import utils
-#from dials.array_family import flex
-#from dxtbx.model import ExperimentList
-#
-##
-#class DataLoad:
-#
-#    def __init__(self, args):
-#        self.args = args
-#        F = utils.open_mtz(args.mtzFile, args.mtzCol)
-#        self.F = F.generate_bijvoet_mates()
-#        self.Finds = self.F.indices()
-#        self.Famp = self.F.data()
-#        self.Fmap = {h: amp for h, amp in zip(self.Finds, self.Famp)}
-#
-#        # experiment list and reflection table
-#        self.Expt = ExperimentList.from_file(args.exptName)[args.exptIdx]
-#
-#        Refs = flex.reflection_table.from_file(args.reflName)
-#        self.Refs = Refs.select(Refs['id'] == args.exptIdx)
-#
-#        # get the pixel data and background estimates near the reflections
-#        self.data = utils.image_data_from_expt(self.Expt)
-#        self.bbox, self.pids, self.tilt_coefs, self.bg_is_good, self.background_image = \
-#            utils.get_roi_background_and_selection_flags(
-#                Refs, self.data, use_robust_estimation=False, shoebox_sz=12,
-#                reject_roi_with_hotpix=False,
-#                pad_for_background_estimation=3, hotpix_mask=self.data < 0, weighted_fit=False)
+        self.bbox
+        """
+        A list/array of the **bounding boxes** (regions of interest) for each
+        reflection on the detector.
+        """
+        self.pids
+        """
+        An array of **panel IDs** indicating which detector panel each reflection
+        is located on.
+        """
+        self.tilt_coefs
+        """
+        The **coefficients** (e.g., from a plane fit) used for the background
+        estimation model near the reflections.
+        """
+        self.bg_is_good
+        """
+        A **boolean array** flagging whether the background estimation was
+        considered successful/reliable for each reflection.
+        """
+        self.background_image
+        """
+        A np.ndarray the same size of self.data of the **estimated background** pixels for the regions
+        of interest and pixels set to -1 otherwise. 
+        """
--- dbex/run_diffbragg.py
+++ dbex/run_diffbragg.py
@@ -12,6 +12,7 @@ from simtbx.command_line.hopper import phil_scope
 from simtbx.diffBragg import hopper_utils, utils, hopper_io
 from simtbx.modeling.forward_models import diffBragg_forward
 
+# TODO : replace the generated files herein with generic tempfiles
 
 def detector_refinement(model_df, Expt, Refs, params):
     new_El = ExperimentList()
@@ -27,18 +28,18 @@ def detector_refinement(model_df, Expt, Refs, params):
     with open("_geom_groups.txt", "w") as o:
         for i_p in range(len(Expt.detector)):
             o.write("%d %d\n" % (i_p, i_p))
-    params3 = deepcopy(params)
-    params3.fix.Fhkl = True
-    params3.refiner.panel_group_file = "_geom_groups.txt"
-    params3.geometry.fix.panel_rotations = [0, 0, 0]
-    params3.geometry.fix.panel_translations = [0, 0, 1]
-    params3.geometry.input_pkl = "_geom_ref.pkl"
-    params3.geometry.save_state_freq = 100000
-    params3.filter_during_refinement.enable = False
-    params3.outdir = "_geom.out"
-    params3.max_process = 1
-    params3.geometry.optimize = True
-    geometry.geom_min(params3)
+    params_geom = deepcopy(params)
+    params_geom.fix.Fhkl = True
+    params_geom.refiner.panel_group_file = "_geom_groups.txt"
+    params_geom.geometry.fix.panel_rotations = [0, 0, 0]
+    params_geom.geometry.fix.panel_translations = [0, 0, 1]
+    params_geom.geometry.input_pkl = "_geom_ref.pkl"
+    params_geom.geometry.save_state_freq = 100000
+    params_geom.filter_during_refinement.enable = False
+    params_geom.outdir = "_geom.out"
+    params_geom.max_process = 1
+    params_geom.geometry.optimize = True
+    geometry.geom_min(params_geom)
     new_det = ExperimentList.from_file("_geom.out/diffBragg_detector.expt")[0].detector
     Expt.detector = new_det
     return Expt
@@ -57,14 +58,14 @@ def run_diffbragg(data_load, devId=0, num_macro=5):
 
     # parameters object that controls hopper refinement
 
-    xtal_refine_phil= os.path.dirname(__file__) + "/xtal_refine.phil"
-    fhkl_refine_phil= os.path.dirname(__file__) + "/fhkl_refine.phil"
+    xtal_refine_phil= os.path.dirname(__file__) + "/dbconfig/xtal_refine.phil"
+    fhkl_refine_phil= os.path.dirname(__file__) + "/dbconfig/fhkl_refine.phil"
     config = open(xtal_refine_phil, "r").read()
     config2 = open(fhkl_refine_phil, "r").read()
     params = phil_scope.fetch(sources=[parse(config)]).extract()
     params.fix.Ndef = False
-    params2 = phil_scope.fetch(sources=[parse(config2)]).extract()
-    for prm in (params, params2):
+    params_fhkl = phil_scope.fetch(sources=[parse(config2)]).extract()
+    for prm in (params, params_fhkl):
         prm.simulator.structure_factors.mtz_name = data_load.args.mtzFile
         prm.roi.hotpixel_mask = data_load.args.maskFile
 
@@ -79,23 +80,21 @@ def run_diffbragg(data_load, devId=0, num_macro=5):
         Expt, _, Modeler, SIM, x = ref_out
         mdl_parm = hopper_utils.get_param_from_x(x, Modeler, as_dict=True)
 
-        for prm in [params, params2]:
+        for prm in [params, params_fhkl]:
             prm.init.Nabc = mdl_parm['Na'], mdl_parm['Nb'], mdl_parm['Nc']
             prm.init.Ndef = mdl_parm['Nd'], mdl_parm['Ne'], mdl_parm['Nf']
             prm.init.G = mdl_parm['scale']
 
         # REFINEMENT OF FHKL
-        ref_out2 = hopper_utils.refine(Expt, data_load.Refs, params2, return_modeler=True, free_mem=True, gpu_device=devId)
-        _, _, Modeler2, SIM2, x2 = ref_out2
-        Fidx_to_asu = {i: hkl for hkl, i in SIM2.asu_map_int.items()}
-        refined = np.where(SIM2.Fhkl_scales != 1)[0]
-        #scale_facs = SIM2.Fhkl_scales[refined]
-        #hkls = [Fidx_to_asu[i] for i in refined]
+        ref_out_fhkl = hopper_utils.refine(Expt, data_load.Refs, params_fhkl, return_modeler=True, free_mem=True, gpu_device=devId)
+        _, _, Modeler_fhkl, SIM_fhkl, x_hkl = ref_out_fhkl
+        Fidx_to_asu = {i: hkl for hkl, i in SIM_fhkl.asu_map_int.items()}
+        refined = np.where(SIM_fhkl.Fhkl_scales != 1)[0]
         new_amps = {}
         for i in refined:
             asu = Fidx_to_asu[i]
             if asu in FMap:
-                scale = SIM2.Fhkl_scales[i]
+                scale = SIM_fhkl.Fhkl_scales[i]
                 new_amp = np.sqrt(scale) * FMap[asu]
                 new_amps[asu] = new_amp
 
@@ -103,38 +102,37 @@ def run_diffbragg(data_load, devId=0, num_macro=5):
             if hkl in new_amps:
                 Famps[i_hkl] = new_amps[hkl]
 
-        F2 = data_load.F.customized_copy(data=Famps)
-        F2.as_mtz_dataset(column_root_label="F").mtz_object().write("_temp.mtz")
-        params.simulator.structure_factors.mtz_name = "_temp.mtz"
-        params.simulator.structure_factors.mtz_column = "F(+),SIGF(+),F(-),SIGF(-)"
-        params2.simulator.structure_factors.mtz_name = "_temp.mtz"
-        params2.simulator.structure_factors.mtz_column = "F(+),SIGF(+),F(-),SIGF(-)"
-        FMap = {h: amp for h, amp in zip(F2.indices(), F2.data())}
+        Fopt = data_load.F.customized_copy(data=Famps)
+        Fopt.as_mtz_dataset(column_root_label="F").mtz_object().write("_temp.mtz")
+        for prm in [params, params_fhkl]:
+            prm.simulator.structure_factors.mtz_name = "_temp.mtz"
+            prm.simulator.structure_factors.mtz_column = "F(+),SIGF(+),F(-),SIGF(-)"
+        FMap = {h: amp for h, amp in zip(Fopt.indices(), Fopt.data())}
         params.filter_during_refinement.enable = False
-        params2.filter_during_refinement.enable = False
+        params_fhkl.filter_during_refinement.enable = False
 
-        model_df = hopper_io.save_to_pandas(x2, Modeler2, SIM2, data_load.args.exptName, params2, Expt, 0,
+        model_df = hopper_io.save_to_pandas(x_hkl, Modeler_fhkl, SIM_fhkl, data_load.args.exptName, params_fhkl, Expt, 0,
                                             data_load.args.reflName, None, 0, write_expt=False, write_pandas=False,
                                             exp_idx=data_load.args.exptIdx)
 
         # REFINEMENT OF DETECTOR
-        Expt = detector_refinement(model_df, Expt, data_load.Refs, params2)
+        Expt = detector_refinement(model_df, Expt, data_load.Refs, params_fhkl)
 
     # with te optimized model from diffBragg, we can predict a model value at every pixel
     energies = [utils.ENERGY_CONV / Expt.beam.get_wavelength()]
-    fluxes = [SIM2.D.flux]
-    mdl_parm = hopper_utils.get_param_from_x(x2, Modeler2, as_dict=True)
+    fluxes = [SIM_fhkl.D.flux]
+    mdl_parm = hopper_utils.get_param_from_x(x_hkl, Modeler_fhkl, as_dict=True)
     Bragg = diffBragg_forward(
-        Expt.crystal, Expt.detector, Expt.beam, F2,
+        Expt.crystal, Expt.detector, Expt.beam, Fopt,
         energies, fluxes,
-        oversample=SIM2.D.oversample,
+        oversample=SIM_fhkl.D.oversample,
         Ncells_abc=(mdl_parm['Na'], mdl_parm['Nb'], mdl_parm['Nc']),
         Ncells_def=(mdl_parm['Nd'], mdl_parm['Ne'], mdl_parm['Nf']),
-        beamsize_mm=SIM2.D.beamsize_mm, device_Id=devId,
+        beamsize_mm=SIM_fhkl.D.beamsize_mm, device_Id=devId,
         spot_scale_override=mdl_parm['scale'],
         cuda=True,
-        num_phi_steps=SIM2.D.phisteps, delta_phi=SIM2.D.phistep_deg,
-        spindle_axis=SIM2.D.spindle_axis, no_Nabc_scale=True
+        num_phi_steps=SIM_fhkl.D.phisteps, delta_phi=SIM_fhkl.D.phistep_deg,
+        spindle_axis=SIM_fhkl.D.spindle_axis, no_Nabc_scale=True
     )
     return Bragg
 
